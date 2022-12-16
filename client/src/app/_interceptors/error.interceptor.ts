6d0fa13166b119eb989d23e7fe35e7c6eba2856d
Nikola Golijanin <nikola.golijanin@vegait.rs>
2022-12-16 14:07:35 +0100

    End of section 7

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router, NavigationExtras } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error:HttpErrorResponse) => {
+        if (error) {
+          switch (error.status) {
+            case 400:
+              if (error.error.errors) {
+                const modalStateErrors = [];
+                for (const key in error.error.errors) {
+                  if (error.error.errors[key]) {
+                    modalStateErrors.push(error.error.errors[key])
+                  }
+                }
+                throw modalStateErrors.flat();
+              } else {
+                alert(error.statusText);
+              }
+              break;
+            case 401:
+              alert(error.statusText);
+              break;
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras = {state: {error: error.error}}
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+            default:
+              alert('Something unexpected went wrong');
+              console.log(error);
+              break;
+          }
+        }
+        throw error;
+      })
+    )
+  }
+}
