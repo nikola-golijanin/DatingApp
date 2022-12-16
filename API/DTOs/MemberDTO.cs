bc4b3deba1b584272a4aacc1072ecfe3fe1fda49
Nikola Golijanin <nikola.golijanin@vegait.rs>
2022-12-16 16:41:24 +0100

    Half of section 8

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -1,14 +1,10 @@
-using API.Extensions;
-
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDTO
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
@@ -18,11 +14,6 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public List<Photo> Photos { get; set; } = new();
-
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        public List<PhotoDTO> Photos { get; set; }
     }
 }
