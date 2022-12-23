c7fb0434cc33b98dc6e774778f5110dc96386511
Nikola Golijanin <nikola.golijanin@vegait.rs>
2022-12-23 16:40:34 +0100

    Half of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,22 +9,44 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   model:any = {}
-
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup = new FormGroup({});
+
 
   constructor(private accountService:AccountService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('',Validators.required),
+      password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')]),
+    })
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
 
   }
 
+  matchValues(matchTo:string) : ValidatorFn {
+    return (control:AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching : true}
+    }
+  }
+
   register(){
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: error => alert(error)
-    })
+    console.table(this.registerForm?.value)
+
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: error => alert(error)
+    // })
   }
 
   cancel(){
